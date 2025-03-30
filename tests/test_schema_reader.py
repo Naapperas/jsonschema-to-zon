48dc71141d2c18c3a10de9485115b8e0a4c30391
Nuno Pereira <nunoafonso2002@gmail.com>
2025-03-30 23:18:20 +0100

    Added basic testing to package

--- tests/test_schema_reader.py
+++ tests/test_schema_reader.py
@@ -1,4 +1,5 @@
-import tempfile
+from unittest.mock import patch, mock_open
+
 import pytest
 
 import jsonschema_to_zon
@@ -6,12 +7,57 @@ import jsonschema_to_zon
 
 @pytest.fixture
 def schema_content_str():
-    return """"""
+    return """{
+    "$id": "https://example.com/conditional-validation-dependentRequired.schema.json",
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "title": "Conditional Validation with dependentRequired",
+    "type": "object",
+    "properties": {
+        "foo": {
+            "type": "boolean"
+        },
+        "bar": {
+            "type": "string"
+        }
+    },
+    "dependentRequired": {
+        "foo": [
+            "bar"
+        ]
+    }
+}"""
 
 
-def test_schema_read_file(schema_content_str):
-    pass
+@pytest.fixture
+def schema_content_file_path():
+    from pathlib import Path
+
+    return Path(__file__).parent / "data" / "simple_schema.json"
 
 
-def test_schema_read_str(schema_content_str):
-    pass
+@pytest.fixture
+def reader():
+    return jsonschema_to_zon.SchemaReader()
+
+
+def test_schema_read_file(reader, schema_content_str, schema_content_file_path):
+
+    with patch("__main__.open", mock_open(read_data=schema_content_str)):
+        try:
+            reader.read_file(schema_content_file_path)
+        except:
+            assert False
+        else:
+            assert (
+                True  # We expect that this operation success without raising an error
+            )
+
+
+def test_schema_read_str(reader, schema_content_str):
+
+    try:
+        reader.read_str(schema_content_str)
+    except:
+        assert False
+    else:
+        assert True
